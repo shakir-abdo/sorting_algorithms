8eed2aa34cbdf7730abb0ec37665abbc7276c3ea
Shakir Abdo <shicolare1@gmail.com>
2023-12-19 20:52:46 +0300

    add test

---
+++ tests/0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[] = {18, 57, 99, 71, 13, 23, 96, 73, 87, 6};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
+}
